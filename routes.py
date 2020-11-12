7c57ab89ed4b1979f0fa547954d5a0608ceb7cac
Sami <sami.seisalo@gmail.com>
2020-11-12 13:01:19 +0200

    login and registering improvements

--- routes.py
+++ routes.py
@@ -1,10 +1,36 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, request, session
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username,password):
+            return redirect("/")
+        else:
+            return "väärä tunnus tai salasana"
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username,password):
+            return redirect("/")
+        else:
+            return "tämä tunnus on jo olemassa"
